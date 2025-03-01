d6bd023ad4868074a65ffbf6217b103df5a6c2ca
Shashidhara K <shashidharak334@gmail.com>
2025-03-01 08:23:19 +0530

    Update App.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React,{ useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Home from './components/Home'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 
 function App() {
