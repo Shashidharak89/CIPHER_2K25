0ba2faf836a4ea7d8a4f3abebfcc746e89581b28
Willscreation <row654321@gmail.com>
2025-03-21 00:22:37 +0530

    images and qoutes

--- src/components/Home-comonents/EventsSection.jsx
+++ src/components/Home-comonents/EventsSection.jsx
@@ -8,6 +8,14 @@ import mimic_mania from './images/event/mimic_mania.jpg';
 import itmanager from './images/event/itmanager.jpeg';
 import tresurehunt from './images/event/tresurehunt.jpeg';
 import gaming from './images/event/gaming.jpeg';
+import vlog from './images/event/vlog.jpeg';
+import video from './images/event/video.jpeg';
+import surpeise from './images/event/surpeise.jpeg';
+import posterdesign from './images/event/posterdesign.jpeg';
+import ontrack from './images/event/ontrack.jpeg';
+import itquiz from './images/event/itquiz.jpeg';
+import itmodel from './images/event/itmodel.jpeg';
+import dance from './images/event/dance.jpg';
 import EventTitle from "../Event-components/EventTitle";
 import design_maze from './images/event/design_maze.jpg';
 
@@ -31,7 +39,7 @@ const eventsData = [
   {
     name: "Round And Round",
     description: "A thrilling dance competition awaits!",
-    img: "/eventimgs/coding.jpeg",
+    img: dance,
     rules: "Filmy-style dance.Each team must have a min 5 - 9 max members.Performance duration should be between 4+1 minutes.Stage setup 2min.Remixes of filmy songs are allowed.The performance will be judged on creativity and the use of props. Team heads must submit their final tracks to the host at least 1 day before the fest.Any inappropriate behaviour or offensive content in performances will lead to disqualification.The judges decisions will be last and final.",
     ehead: "Thejadri",
     headno: "87625 68184"
@@ -54,23 +62,23 @@ const eventsData = [
   },
   {
     name: "Trail Trivia",
-    description: "Test your coding skills against the best!",
-    img: "/eventimgs/coding.jpeg",
+    description: "Ready to play dare to stay",
+    img: itquiz,
     rules: "Each team will consist of 4 participants, with 2 participants coming from each class(minimum one boy or girl).There will be no eliminations during the first day's round.The top 4 teams will be selected for the finals.Points will be reset to zero for the finals.No mobile phones or smart watches are allowed during the event.GENERAL RULES and updates regarding rounds will be provided prior to the round.Participants cannot be involved in the treasure hunt, photography, or video events.Taking help from non-participants will lead to the immediate disqualification of the team.",
     ehead: "Sharath",
     headno: "82175 79389"
   },
   {
     name: "Squid Innovate",
-    description: "Test your coding skills against the best!",
-    img: "/eventimgs/coding.jpeg",
+    description: "Turning Imagination into Innovation with IT Models!",
+    img: itmodel,
     rules: "Each team must consist of 4 members.(2- I BCA , 2-II BCA). Ready-made models or commercially available models are strictly not permitted. If found, the team will be disqualified. Usage of laptops, mobile phones, and internet is allowed. Teams must prepare their models in advance and present them on the scheduled exhibition day. Each team will have 5-10 minutes for their presentation, followed by a Q&A session. Lots picking for exhibition will be done on the previous day of the event. All members are expected to maintain decorum and discipline throughout the event.",
     ehead: "Rashmitha",
     headno: "96112 94611"
   },
   {
     name: "The Survival hunt",
-    description: "Test your coding skills against the best!",
+    description: "Think Smart, Move Fast – The Treasure Awaits!",
     img: tresurehunt,
     rules: "Make a team of 2 memebers, Each team must include one participant from each year. Taking help from non-participants will lead to the immediate disqualification of the team. Participants cannot be involved in the IT Quiz, Photography & videography events. GENERAL RULES for each round will be updated prior to the start of the round.",
     ehead: "Sanath Shetty",
@@ -79,23 +87,23 @@ const eventsData = [
   },
   {
     name: "Risk It All",
-    description: "Test your coding skills against the best!",
-    img: "/eventimgs/coding.jpeg",
+    description: "Expect the Unexpected – Are You Ready?",
+    img: surpeise,
     rules: "There will be two participants ( 1 Boy, 1 Girl) There will be 6 rounds. Event will be conducted on the spot. There is elimination in every round. Judge’s decision is final.",
     ehead: "Anusha Shetty",
     headno: "83102 21587"
   },
   {
     name: "Squilog",
-    description: "Test your coding skills against the best!",
-    img: "/eventimgs/coding.jpeg",
+    description: "Lights, Camera, Action – Your Story Awaits!",
+    img: vlog,
     rules: "The competition duration is strictly 1 minute.Each team must consist of exactly 2 participants.Please ensure that no vulgarity is allowed Respect Privacy and GENERAL RULES avoid filming in restricted areas.Participants are allowed to shoot their vlogs between 8:00 am - 9:30 am and 3:45 pm - 4:30 pm.The topic and its corresponding guidelines for the vlog will be provided soon.The vlog must be presented in either Kannada or English.The decision of the judges and organisers shall be final and binding on all participants.",
     ehead: "Mahammad Razik",
     headno: "73376 16956"
   },
   {
     name: "Mimic Mania",
-    description: "Test your coding skills against the best!",
+    description: "Act It Out, Guess It Right, Win the Fight!",
     img: mimic_mania,
     rules: "There will be two participants.There will be multiple rounds. There must be two players on each team—one to play and the other to assist.Players have to finish the assignment in the allotted time for each round. The team receives a point if they properly estimate the task within the allotted time. Familiarity with web series and films(all language), as well as proficiency with IT and a few common expressions.Elimination is included.",
     ehead: "Kavya",
@@ -103,24 +111,24 @@ const eventsData = [
   },
   {
     name: "Run For Survival",
-    description: "Test your coding skills against the best!",
-    img: "/eventimgs/coding.jpeg",
+    description: "Race to Glory – Every Second Counts!",
+    img: ontrack,
     rules: "Participants-1(anyone can participate).Event will be held on track (outdoor).There will be 2 rounds.Elimination will be done in each round.Properties will be provided by the event handlers.",
     ehead: "Shawn veigas",
     headno: "8050365040"
   },
   {
     name: "Sutter Bug",
-    description: "Test your coding skills against the best!",
-    img: "/eventimgs/coding.jpeg",
+    description: "Every Shot Tells a Story – What’s Yours?",
+    img: video,
     rules: "Two members from a team.  Topics will be given on the spot.  Only mobile phones are allowed for [photography videography] .For photography editing is not allowed. Video length must be 30s.  Participants of photography  & videography are not allowed to take part in Tresure Hunt, It Quiz, It manager.",
     ehead: "Vineeth Hv",
     headno: "99017 92801"
   },
   {
     name: "Squid Sketch",
-    description: "Test your coding skills against the best!",
-    img: "/eventimgs/coding.jpeg",
+    description: "Design the Future, One Poster at a Time!",
+    img: posterdesign,
     rules: "Time allotted: 1 hour.Number of participants – 2The theme of the content is given on the spot.Laptops are provided by event handlers.No participants are allowed to bring mobile phones.Internet facility will be provided (If required).Participants are asked to gather before 10 min of the event.Each teams are allowed to create only one poster.Pre-edited posters are not allowed.Using templates in Canva are not allowed. If found the team will be disqualified.",
     ehead: "Ashwin Rodrigues",
     headno: "8317383882"
